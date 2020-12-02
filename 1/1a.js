b07ae21f0ef48277bbe434a8d965f41517233537
mdunisch <martin.dunisch@gmail.com>
2020-12-02 10:14:42 +0100

    adding prettier + eslint

--- 1/1a.js
+++ 1/1a.js
@@ -1,15 +1,17 @@
 import { readFileSync } from 'fs';
 
-const input = readFileSync('input.txt', 'utf8').split('\n').map(i => parseInt(i, 10));
+const input = readFileSync('input.txt', 'utf8')
+  .split('\n')
+  .map((i) => parseInt(i, 10));
 
 // 1. create matrix
-let numberMatrix = input.map(i => input.map(x => [i, x]));
+let numberMatrix = input.map((i) => input.map((x) => [i, x]));
 
 // 2. flat and remove duplicate
 numberMatrix = numberMatrix.flat().filter(([x, y]) => x !== y);
 
 // 3. find right element
-const found = numberMatrix.find(([x, y]) => x+y === 2020);
+const found = numberMatrix.find(([x, y]) => x + y === 2020);
 
 // 4. solution
 console.log(found[0] * found[1]);
--- 2/2a.js
+++ 2/2a.js
@@ -3,24 +3,24 @@ import { readFileSync } from 'fs';
 const input = readFileSync('input.txt', 'utf8').split('\n');
 
 // 1. create json of passwords
-const passwords = input.map(line => {
-    const [ num, letter, password ] = line.split(' ');
-    const [ min, max ] = num.split('-');
+const passwords = input.map((line) => {
+  const [num, letter, password] = line.split(' ');
+  const [min, max] = num.split('-');
 
-    return {
-        min: parseInt(min, 10),
-        max: parseInt(max, 10),
-        letter: letter.replace(':', ''),
-        password
-    }
+  return {
+    min: parseInt(min, 10),
+    max: parseInt(max, 10),
+    letter: letter.replace(':', ''),
+    password,
+  };
 });
 
 // 2. check vaild func
-function isPasswordVaild(passwordInput){
-    const letterRegex = new RegExp(passwordInput.letter,'gi');
-    const numOccurs = passwordInput.password.match(letterRegex)?.length;
+function isPasswordVaild(passwordInput) {
+  const letterRegex = new RegExp(passwordInput.letter, 'gi');
+  const numOccurs = passwordInput.password.match(letterRegex)?.length;
 
-    return numOccurs >= passwordInput.min && numOccurs <= passwordInput.max;
+  return numOccurs >= passwordInput.min && numOccurs <= passwordInput.max;
 }
 
 // 3. count vaild passwords in json
