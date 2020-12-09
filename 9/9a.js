c3622b85686b4389c88612a55ff8b50f406f358f
mdunisch <martin.dunisch@gmail.com>
2020-12-09 16:56:52 +0100

    better day 9 part 1

--- 9/9a.js
+++ 9/9a.js
@@ -8,19 +8,7 @@ const numbers = input.map((i) => parseInt(i, 10));
 // 2. function to check if number array item is valid
 function checkItem(index) {
   const elementsToSum = numbers.slice(index - 25, index);
-  const options = elementsToSum.map((i, index) => elementsToSum.map((number) => [elementsToSum[index], number]));
-  const clearedoptions = options
-    .flat(1)
-    .map((numbers) => {
-      const [a, b] = numbers;
-      if (a > b) {
-        return [b, a];
-      }
-      return [a, b];
-    })
-    .filter(([a, b]) => a != b);
-
-  return clearedoptions.some(([a, b]) => a + b === numbers[index]);
+  return elementsToSum.some((a) => elementsToSum.some((b) => b !== a && b + a === numbers[index]));
 }
 
 // 3. check all Elements until found wrong
